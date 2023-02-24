ce13193e25166b95bdac6babdd0705fc3498d639
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2023-02-25 00:15:56 +0200

    added program

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
@@ -20,7 +33,8 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     }
 }
 
-invokeAction({ action: "list" });
+// invokeAction({ action: "list" });
 // invokeAction({ action: "get", id: "10" });
 // invokeAction({ action: "add", name: "Denis Zabolotnyi", email: "nati.zajka@gmail.com", phone: "(093) 004-3110" });
 // invokeAction({ action: "remove", id: "uWEY3NyjJSiTI0Usi-V6D" });
+invokeAction(argv);
